533ff65d050ab70bfc049c4fadaafd8a7265cf82
fredacha <ramazanov-zalim@mail.ru>
2024-04-05 13:53:49 +0300

    dev01

--- develop/dev01/main.go
+++ develop/dev01/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/beevik/ntp"
+)
+
+func GetTime() {
+	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	fmt.Println(time)
+}
+
 func main() {
-	
+	GetTime()
 }
